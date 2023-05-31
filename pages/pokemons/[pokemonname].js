6a4d3cfd9162052792f7fe67922114ecaecd6c6c
Jconneely95 <jack.conneely12@gmail.com>
2023-05-31 17:08:08 +0100

    Final

--- pages/pokemons/[pokemonname].js
+++ pages/pokemons/[pokemonname].js
@@ -20,64 +20,85 @@ const typeColors = {
   ground: 'bg-red-900',
   ghost: 'bg-gray-200',
   dragon: 'bg-purple-800',
-  water: 'bg-blue-700',
   steel: 'bg-gray-400',
   poison: 'bg-green-800',
-  mouse: 'bg-purple-700'
+  mouse: 'bg-purple-700',
 };
 
-export const getPokemon = async () => {
-  const response = await fetch('http://localhost:8000/pokemon/?sort=type');
-  const data = await response.json();
-  return data;
-};
-
-export default function PokemonPage({ details, stats }) {
+export default function PokemonPage({ pokemon, stats }) {
   const router = useRouter();
-
-  <PokemonList />
+  const { pokemonname } = router.query;
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Pokemon - {details && details.name}</h1>
-      <div className="flex items-center justify-center mb-6">
-        <Image src={details && details.imageURL} width={200} height={200} alt={details && details.name} />
-      </div>
+      <h1 className="text-3xl font-bold mb-4">Pokemon - {pokemonname}</h1>
+      <div className="flex justify-center items-center">
+          {pokemon && (
+            <img
+              src={`/images/pokemon/${pokemonname}.png`}
+              alt={pokemonname}
+              width={300}
+              height={300}
+              className="rounded-lg"
+            />
+          )}
+        </div>
       <div className="grid grid-cols-2 gap-4">
         <div>
           <h2 className="text-xl font-semibold mb-2">Details</h2>
           <p>
-            <span className="font-bold">Height:</span> {stats && stats.height_m} Metres
+            <span className="font-bold text-gray-700">Generation:</span> {pokemon && pokemon.data.generation}
           </p>
           <p>
-            <span className="font-bold">Weight:</span> {stats && stats.weight_kg} Kilograms
+            <span className="font-bold text-gray-700">Classification:</span> {pokemon && pokemon.data.classification}
+          </p>
+          <p>
+            <span className="font-bold text-gray-700">Type:</span>{" "}
+            <span
+              className={`px-3 py-1 rounded text-white text-sm font-semibold ${typeColors[pokemon && pokemon.data.type1]}`}
+            >
+              {pokemon && pokemon.data.type1 && pokemon.data.type1.charAt(0).toUpperCase() + pokemon.data.type1.slice(1)}
+            </span>
+          </p>
+          <p>
+            <span className="font-bold text-gray-700">Sub Type:</span>{" "}
+            <span
+              className={`px-3 py-1 rounded text-white text-sm font-semibold ${typeColors[pokemon && pokemon.data.type2]}`}
+            >
+              {pokemon && pokemon.data.type2 && pokemon.data.type2.charAt(0).toUpperCase() + pokemon.data.type2.slice(1)}
+            </span>
+          </p>
+          <p>
+            <span className="font-bold text-gray-700">Height:</span> {stats && stats.height_m} Metres
+          </p>
+          <p>
+            <span className="font-bold text-gray-700">Weight:</span> {stats && stats.weight_kg} Kilograms
           </p>
         </div>
         <div>
           <h2 className="text-xl font-semibold mb-2">Stats</h2>
           <p>
-            <span className="font-bold">Speed:</span> {stats && stats.speed}
+            <span className="font-bold text-gray-700">Speed:</span> {stats && stats.speed}
           </p>
           <p>
-            <span className="font-bold">Attack:</span> {stats && stats.attack}
+            <span className="font-bold text-gray-700">Attack:</span> {stats && stats.attack}
           </p>
           <p>
-            <span className="font-bold">Defense:</span> {stats && stats.defense}
+            <span className="font-bold text-gray-700">Defense:</span> {stats && stats.defense}
           </p>
           <p>
-            <span className="font-bold">HP:</span> {stats && stats.hp}
+            <span className="font-bold text-gray-700">HP:</span> {stats && stats.hp}
           </p>
         </div>
       </div>
-      {details && details.types && (
+      {pokemon && pokemon.types && (
         <div className="mt-8">
           <h2 className="text-xl font-semibold mb-2">Type</h2>
           <div className="flex space-x-2">
-            {details.types.map((type, index) => (
+            {pokemon.types.map((type, index) => (
               <span
                 key={index}
-                className="px-3 py-1 rounded text-white text-sm font-semibold"
-                style={{ backgroundColor: typeColors[type.toLowerCase()] }}
+                className={`px-3 py-1 rounded text-white text-sm font-semibold ${typeColors[type.toLowerCase()]}`}
               >
                 {type}
               </span>
@@ -89,25 +110,25 @@ export default function PokemonPage({ details, stats }) {
   );
 }
 
-
-
 export async function getServerSideProps(context) {
-  const pokemonName = context.params.pokemonname;
+  const { pokemonname } = context.params;
 
-  // Fetch Pokemon details from your backend API
-  const detailsResponse = await fetch(`http://127.0.0.1:8000/pokemon/${pokemonName}`);
-  const detailsData = await detailsResponse.json();
+  const pokemonResponse = await fetch(`http://127.0.0.1:8000/pokemon/${pokemonname}`);
+  const pokemonData = await pokemonResponse.json();
+  console.log('Pokemon Data:', pokemonData);
 
-  // Fetch Pokemon stats from your backend API
-  const statsResponse = await fetch(`http://127.0.0.1:8000/pokemon/stats/${pokemonName}`);
+  const statsResponse = await fetch(`http://127.0.0.1:8000/pokemon/stats/${pokemonname}`);
   const statsData = await statsResponse.json();
+  console.log('Stats Data:', statsData);
 
   return {
     props: {
-      details: detailsData,
+      pokemon: pokemonData,
       stats: statsData,
     },
   };
 }
+
+
 
 
--- pages/pokemons/index.js
+++ pages/pokemons/index.js
@@ -1,46 +1,135 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
+import ReactPaginate from 'react-paginate';
 import PokemonList from '@/components/PokemonList';
 import Title from '@/components/Title';
 
 export default function Pokemons({ pokemonsList }) {
   const [searchQuery, setSearchQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 22;
   const filteredPokemons = pokemonsList.filter((pokemon) =>
     pokemon.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
+  const pageCount = Math.ceil(filteredPokemons.length / itemsPerPage);
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
+
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
+  const paginatedPokemons = filteredPokemons.slice(
+    currentPage * itemsPerPage,
+    (currentPage + 1) * itemsPerPage
+  );
 
   return (
     <>
       <Head>
         <title>Pokedex Home</title>
       </Head>
-      <div className="bg-blue-400 min-h-screen flex flex-col items-center justify-center">
-        <Title content="Choose your Pokemon!" className="text-4xl font-bold text-white mb-4" />
+      <div className="bg-gray-900 text-white min-h-screen">
+        <div className="bg-gray-800 py-8">
+          <div className="container mx-auto px-4">
+            <Title content="Choose your Pokemon!" className="text-5xl font-bold mb-4 text-center" />
 
-        <div className="mb-4">
-          <label htmlFor="searchInput" className="text-white font-bold">
-            Search Pokémon by name:
-          </label>
-          <input
-            type="text"
-            id="searchInput"
-            value={searchQuery}
-            onChange={handleSearchChange}
-            placeholder="Enter Pokémon name"
-            className="block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-          />
+            <div className="flex justify-center">
+              <div className="relative w-full max-w-md">
+                <label htmlFor="searchInput" className="text-gray-200 font-bold mb-2">
+                  Search Pokémon by name:
+                </label>
+                <input
+                  type="text"
+                  id="searchInput"
+                  value={searchQuery}
+                  onChange={handleSearchChange}
+                  placeholder="Enter Pokémon name"
+                  className="w-full p-4 bg-gray-900 text-white focus:outline-none"
+                />
+              </div>
+            </div>
+          </div>
         </div>
 
-        <PokemonList pokemons={filteredPokemons} />
+        <div className="container mx-auto px-4 py-8">
+          <div className="bg-gray-800 rounded-lg shadow-lg p-4">
+            <div className="grid grid-cols-2 gap-4">
+              {paginatedPokemons.map((pokemon) => (
+                <div key={pokemon.name} className="flex items-center">
+                  <Link href={`/pokemons/${pokemon.name}`}>
+                    <img
+                      src={`/images/pokemon/${pokemon.name}.png`}
+                      alt={pokemon.name}
+                      className="w-12 h-12 mr-4 cursor-pointer"
+                    />
+                  </Link>
+                  <Link href={`/pokemons/${pokemon.name}`}>
+                    <div className="pokemon-name text-white cursor-pointer">{pokemon.name}</div>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex justify-center mt-4">
+            <ReactPaginate
+              previousLabel={'Previous'}
+              nextLabel={'Next'}
+              breakLabel={'...'}
+              breakClassName={'break-me'}
+              pageCount={pageCount}
+              marginPagesDisplayed={1}
+              pageRangeDisplayed={1}
+              onPageChange={handlePageChange}
+              containerClassName={'pagination-container'}
+              activeClassName={'active'}
+              pageClassName={'page-item'}
+              pageLinkClassName={'page-link'}
+              previousClassName={'page-link'}
+              nextClassName={'page-link'}
+              disabledClassName={'disabled'}
+            />
+          </div>
+        </div>
       </div>
 
       <style jsx>{`
         .pokemon-name {
-          font-weight: 900;
+          font-weight: bold;
+          color: white;
+        }
+
+        .pagination-container {
+          display: flex;
+          justify-content: center;
+          margin-top: 1rem;
+        }
+
+        .page-item {
+          list-style-type: none;
+          margin: 0 0.5rem;
+        }
+
+        .page-link {
+          display: inline-block;
+          padding: 0.5rem 1rem;
+          color: white;
+          background-color: gray;
+          border: none;
+          cursor: pointer;
+        }
+
+        .active .page-link {
+          background-color: blue;
+        }
+
+        .disabled .page-link {
+          opacity: 0.5;
+          pointer-events: none;
         }
       `}</style>
     </>
@@ -66,6 +155,28 @@ export async function getServerSideProps(context) {
     };
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
